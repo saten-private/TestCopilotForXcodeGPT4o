249ea54b925f61d5dbf66b97466997462c37e49f
saten-private <38902911+saten-private@users.noreply.github.com>
2024-08-02 19:04:36 +0900

    first commit

---
+++ TestCopilotForXcodeGPT4o/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCopilotForXcodeGPT4o
+//
+//  Created by AIWork on 2024/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
